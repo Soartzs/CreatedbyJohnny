2b3e7c26e85e9c99866873bcc31fa2764eb7329e
Soartzs <soartzs.jonathan.z@gmail.com>
2026-02-11 17:35:57 +0100

    Summary

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -22,7 +22,7 @@ const AboutSection = () => {
           <div className="relative lg:col-span-2">
             <div className="relative overflow-hidden border border-border/50">
               <img
-                src={import.meta.env.BASE_URL + "src/assets/self.jpeg"}
+                src={self-portrait}
                 alt="Portrait"
                 className="aspect-[3/4] w-full object-cover"
               />
